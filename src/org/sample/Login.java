e230fa8e1ed82ada1e44bf53057226f6f8eaf8a5
venkat9898 <venky15.05s@gmail.com>
2021-09-08 09:38:29 +0530

    userName Functionality

--- src/org/sample/Login.java
+++ src/org/sample/Login.java
@@ -1,5 +1,11 @@
 package org.sample;
 
 public class Login {
-
+ public void userName() {
+	System.out.println("userName");
+}
+ public void Lastname() {
+		System.out.println("Lastname");
+	}
+ 
 }
